67abe1a570e7a3cf23a0399ffa95b241ed0282b7
Satvik Sawhney <satviksawhney@Satviks-MacBook-Pro.local>
2024-03-30 16:15:55 +0530

    Retry

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/PomodoroTimer/",
   plugins: [react()],
-  base: "/PomodoroTimer/"
 })
